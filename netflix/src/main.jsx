a541393f9b50dfe0c52317f08f04d9cc95b2feb1
Chandan8585 <chandanwingshr@gmail.com>
2023-08-01 10:05:35 +0530

    checking 7

--- netflix/src/main.jsx
+++ netflix/src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.scss'
-import { Store } from './Store/Store.js'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.scss';
+import { Store } from './Store/Store.js';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
+
