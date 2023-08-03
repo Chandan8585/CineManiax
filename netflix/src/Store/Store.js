f0aff05d13323f9c4b9b1d71c8c59770d223ca86
Chandan8585 <chandanwingshr@gmail.com>
2023-08-03 10:34:49 +0530

    checking

--- netflix/src/Store/Store.js
+++ netflix/src/Store/Store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import HomeSlice from "./HomeSlice.js";
+import HomeReducer from "./HomeSlice.js";
 export const Store = configureStore({
   reducer: {
-    home: HomeSlice
+    home: HomeReducer
 
   },
 });
